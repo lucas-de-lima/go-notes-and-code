f54f222adb90f4b6a9d10a58fc9c8304143702ee
lucas-de-lima <lucasramosdelima@gmail.com>
2025-08-26 20:02:28 -0300

    add tipo function to identify data types and implement main function for testing

---
+++ geral/switch3/switch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func tipo(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "inteiro"
+	case float32, float64:
+		return "real"
+	case string:
+		return "string"
+	case func():
+		return "função"
+	default:
+		return "não sei"
+	}
+}
+
+func main() {
+	fmt.Println(tipo(""))
+	fmt.Println(tipo(1))
+	fmt.Println(tipo(float32(2.4)))
+	fmt.Println(tipo((2.0)))
+	fmt.Println(tipo(func() {}))
+	fmt.Println(tipo(time.Now()))
+}
